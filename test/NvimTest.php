8de74d1b8bd90221af7a97dc40310ca4d884d2dc
Aleksei Khudiakov <aleksey@xerkus.pro>
2016-03-25 00:10:26 +1000

    Add temporary test to assert pthreads are available

--- test/NvimTest.php
+++ test/NvimTest.php
@@ -6,5 +6,8 @@ use PHPUnit_Framework_TestCase;
 
 class NvimTest extends PHPUnit_Framework_TestCase
 {
-
+    public function testHasTreaded()
+    {
+        self::assertTrue(class_exists('Thread'));
+    }
 }
