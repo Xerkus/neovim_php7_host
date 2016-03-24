1d169740653b2adbd2870f42b51f28e72f702b18
Aleksei Khudiakov <aleksey@xerkus.pro>
2016-03-24 22:09:40 +1000

    Add initial Plugin interface

---
+++ src/Plugin/Plugin.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace Xerkus\Neovim\Plugin;
+
+use Xerkus\Neovim\Nvim;
+
+/**
+ * Plugin marker interface.
+ */
+interface Plugin
+{
+    /**
+     * Human readable plugin name
+     */
+    public function getName() : string;
+
+    /**
+     * Returns rpc handlers
+     *
+     * @return RpcHandler\Handler[]
+     */
+    public function getRpcHandlers() : array;
+
+    /**
+     * Shutdown callback, nvim might be gone by the time it is called
+     */
+    public function onShutdown();
+}
