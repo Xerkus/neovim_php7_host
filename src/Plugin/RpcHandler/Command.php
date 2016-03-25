570e9f444ea6fde75ea1e4259a8f17d7334ef4cf
Aleksei Khudiakov <aleksey@xerkus.pro>
2016-03-26 03:33:18 +1000

    Add Command rpc handler

---
+++ src/Plugin/RpcHandler/Command.php
@@ -0,0 +1,155 @@
+<?php
+declare(strict_types=1);
+
+namespace Xerkus\Neovim\Plugin\RpcHandler;
+
+use RuntimeException;
+use InvalidArgumentException;
+
+/**
+ * @Annotation
+ * @Target("METHOD")
+ */
+final class Command implements RpcSpec
+{
+    use RpcSpecTrait;
+
+    private $opts = [];
+
+    /**
+     *Array of values as passed from annotations reader
+     *
+     */
+    public function __construct(array $values)
+    {
+        $name = $values['name'] ?? $values['value'] ?? null;
+        if ($name === null) {
+            throw new RuntimeException('Command name is required');
+        }
+        if (!empty($values['range']) && !empty($values['count'])) {
+            throw new RuntimeException('Options range and count are mutually exclusive');
+        }
+
+        $this->setName($name);
+        $this->setSync($values['sync'] ?? false);
+        $this->setNargs($values['nargs'] ?? '0');
+        $this->setComplete($values['complete'] ?? null);
+        $this->setRange($values['range'] ?? false);
+        $this->setCount($values['count'] ?? false);
+        $this->setBang($values['bang'] ?? false);
+        $this->setRegister($values['register'] ?? false);
+        $this->setEval($values['eval'] ?? null);
+    }
+
+    /**
+     *  Create nvim function spec from positional parameters
+     */
+    public static function createCommand(
+        string $name,
+        bool $sync = false,
+        string $nargs = '0',
+        string $complete = null,
+        $range = false,
+        $count = false,
+        bool $bang = false,
+        bool $register = false,
+        string $eval = null
+    ) {
+        $values = [
+            'name' => $name,
+            'sync' => $sync,
+            'nargs' => $nargs,
+            'complete' => $complete,
+            'range' => $range,
+            'count' => $count,
+            'bang' => $bang,
+            'register' => $register,
+            'eval' => $eval,
+        ];
+        return new self($values);
+    }
+
+    public function getType() : string
+    {
+        return 'command';
+    }
+
+    protected function prepareOpts() : array
+    {
+        return $this->opts;
+    }
+
+    public function getShouldExport() : bool
+    {
+        return true;
+    }
+
+    private function setName(string $name)
+    {
+        if (empty($name)) {
+            throw new RuntimeException('Invalid command name');
+        }
+        $this->name = $name;
+    }
+
+    private function setSync(bool $sync)
+    {
+        $this->sync = $sync;
+    }
+
+    private function setNargs(string $nargs)
+    {
+        $validValues = ['0', '1', '*', '?', '+'];
+
+        if (!in_array($nargs, $validValues)) {
+            throw new InvalidArgumentException(
+                'Nargs value can only be one of ' . implode(', ', $validValues)
+            );
+        }
+        $this->opts['nargs'] = $nargs;
+    }
+
+    private function setComplete(string $complete = null)
+    {
+        if ($complete === null) {
+            return;
+        }
+        $this->opts['complete'] = $complete;
+    }
+
+    private function setRange($range)
+    {
+        if ($range === false) {
+            return;
+        }
+        // bool or string
+        $this->opts['range'] = $range;
+    }
+
+    private function setCount($count)
+    {
+        if ($count === false) {
+            return;
+        }
+        //bool or string
+        $this->opts['count'] = $count;
+    }
+
+    private function setBang(bool $bang)
+    {
+        $bang && $this->opts['bang'] = true;
+    }
+
+
+    private function setRegister(bool $register)
+    {
+        $this->opts['register'] = $register;
+    }
+
+    private function setEval(string $eval = null)
+    {
+        if (!empty($eval)) {
+            $this->opts['eval'] = $eval;
+        }
+    }
+}
---
+++ test/Plugin/RpcHandler/CommandTest.php
@@ -0,0 +1,247 @@
+<?php
+declare(strict_types=1);
+
+namespace XerkusTest\Neovim\Plugin\RpcHandler;
+
+use InvalidArgumentException;
+use PHPUnit_Framework_TestCase;
+use RuntimeException;
+use TypeError;
+use Xerkus\Neovim\Plugin\RpcHandler\Command;
+
+/**
+ *
+ * @coversDefaultClass Xerkus\Neovim\Plugin\RpcHandler\Command
+ * @covers ::<!public>
+ */
+class CommandTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers ::__construct
+     * @covers ::getType
+     * @covers ::getName
+     * @covers ::getIsSync
+     * @covers ::getSpecArray
+     */
+    public function testAllValuesAreProperlySet()
+    {
+        $cmd = new Command([
+            'name' => 'TestCommand',
+            'sync' => true,
+            'nargs' => '+',
+            'complete' => 'dir',
+            'range' => true,
+            'count' => false,
+            'bang' => true,
+            'register' => true,
+            'eval' => 'someEval()',
+        ]);
+        self::assertEquals('command', $cmd->getType());
+        self::assertEquals('TestCommand', $cmd->getName());
+        self::assertTrue($cmd->getIsSync());
+        self::assertEquals([
+            'type' => 'command',
+            'name' => 'TestCommand',
+            'sync' => true,
+            'opts' => [
+                'range' => true,
+                'nargs' => '+',
+                'complete' => 'dir',
+                'range' => true,
+                'bang' => true,
+                'register' => true,
+                'eval' => 'someEval()'
+            ]
+        ], $cmd->getSpecArray());
+    }
+
+    /**
+     * @covers ::createCommand
+     * @covers ::getType
+     * @covers ::getName
+     * @covers ::getIsSync
+     * @covers ::getSpecArray
+     */
+    public function testNamedConstructorSetsAllValues()
+    {
+        $cmd = Command::createCommand(
+            'TestCommand',
+            true,
+            '+',
+            'dir',
+            'N',
+            false,
+            true,
+            true,
+            'someEval()'
+        );
+        self::assertEquals('command', $cmd->getType());
+        self::assertEquals('TestCommand', $cmd->getName());
+        self::assertTrue($cmd->getIsSync());
+        self::assertEquals([
+            'type' => 'command',
+            'name' => 'TestCommand',
+            'sync' => true,
+            'opts' => [
+                'range' => true,
+                'nargs' => '+',
+                'complete' => 'dir',
+                'range' => 'N',
+                'bang' => true,
+                'register' => true,
+                'eval' => 'someEval()'
+            ]
+        ], $cmd->getSpecArray());
+    }
+
+    /**
+     * @covers ::__construct
+     */
+    public function testCountAndRangeCantBeUsedTogether()
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Options range and count are mutually exclusive');
+        $cmd = new Command([
+            'name' => 'TestCommand',
+            'range' => true,
+            'count' => true,
+        ]);
+    }
+
+    /**
+     * @covers ::__construct
+     */
+    public function testCountIsSet()
+    {
+        $cmd = new Command(['name' => 'TestCommand', 'count' => 'N']);
+        self::assertArrayHasKey('count', $cmd->getSpecArray()['opts']);
+        self::assertEquals('N', $cmd->getSpecArray()['opts']['count']);
+    }
+
+    /**
+     * @covers ::__construct
+     */
+    public function testNargsOutOfRangeValue()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessageRegExp('/Nargs value can only be one of/');
+        $cmd = new Command([
+            'name' => 'TestCommand',
+            'nargs' => '$',
+        ]);
+    }
+
+    /**
+     * @covers ::getMethodName
+     */
+    public function testMethodNameIsCombinationOfTypeAndName()
+    {
+        $cmd = new Command(['name' => 'TestCommand']);
+        self::assertEquals('command:TestCommand', $cmd->getMethodName());
+    }
+
+    /**
+     * @covers ::__construct
+     */
+    public function testCommandNameMustBeString()
+    {
+        $this->expectException(TypeError::class);
+        $this->expectExceptionMessageRegExp('/must be of the type string/');
+        new Command(['name' => true]);
+    }
+
+    /**
+     * @covers ::__construct
+     */
+    public function testCommandNameMustNotBeEmpty()
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Invalid command name');
+        new Command(['name' => '']);
+    }
+
+    /**
+     * @covers ::__construct
+     */
+    public function testCommandNameIsRequired()
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Command name is required');
+        new Command([]);
+    }
+
+    /**
+     * @covers ::__construct
+     */
+    public function testSyncMustBeBool()
+    {
+        $this->expectException(TypeError::class);
+        $this->expectExceptionMessageRegExp('/must be of the type bool/');
+        new Command(['name' => 'TestCommand', 'sync' => 1]);
+    }
+
+    /**
+     * @covers ::__construct
+     */
+    public function testEvalMustBeString()
+    {
+        $this->expectException(TypeError::class);
+        $this->expectExceptionMessageRegExp('/must be of the type string/');
+        new Command(['name' => 'TestCommand', 'eval' => 1]);
+    }
+
+    /**
+     * @covers ::__construct
+     */
+    public function testEvalIsNullable()
+    {
+        $cmd = new Command(['name' => 'TestCommand', 'eval' => null]);
+        self::assertArrayNotHasKey('eval', $cmd->getSpecArray()['opts']);
+    }
+
+    /**
+     * @covers ::getPluginPath
+     */
+    public function testPluginPathIsNullByDefault()
+    {
+        $cmd = new Command(['name' => 'TestCommand']);
+        self::assertNull($cmd->getPluginPath());
+    }
+
+    /**
+     * @covers ::getPluginPath
+     * @covers ::withPluginPath
+     */
+    public function testWithPluginPathProducesNewCopy()
+    {
+        $cmd = new Command(['name' => 'TestCommand']);
+        $new = $cmd->withPluginPath('/test/path/plugin.php');
+        self::assertNotSame($new, $cmd);
+        self::assertNull($cmd->getPluginPath());
+        self::assertInstanceOf(Command::class, $new);
+        self::assertEquals('/test/path/plugin.php', $new->getPluginPath());
+    }
+
+    /**
+     * @covers ::getMethodName
+     * @covers ::withPluginPath
+     */
+    public function testPluginPathIsPrefixedToMethodName()
+    {
+        $cmd = new Command(['name' => 'TestCommand']);
+        $cmd = $cmd->withPluginPath('/test/path/plugin.php');
+        self::assertEquals(
+            '/test/path/plugin.php:command:TestCommand',
+            $cmd->getMethodName()
+        );
+    }
+
+    /**
+     * @covers ::getShouldExport
+     */
+    public function testIsExportedToNeovim()
+    {
+        $cmd = new Command(['name' => 'TestCommand']);
+        self::assertTrue($cmd->getShouldExport());
+    }
+}
