733bfd21eb72ee622a616d798def2151de5abd1e
Aleksei Khudiakov <aleksey@xerkus.pro>
2016-03-25 17:52:48 +1000

    Add neovim function spec class with tests

--- src/Plugin/RpcHandler/Func.php
+++ src/Plugin/RpcHandler/Func.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace Xerkus\Neovim\Plugin\RpcHandler;
 
+use RuntimeException;
+
 /**
  * @Annotation
  * @Target("METHOD")
@@ -17,17 +19,37 @@ final class Func implements RpcSpec
     private $pluginPath;
 
     /**
-     *Array of values passed from annotation reader
+     *Array of values as passed from annotations reader
      *
      */
     public function __construct(array $values)
     {
-        $name = $values['name'] ?? ($values['value'] ?? null);
-        if (!is_string($name) || empty($name)) {
-            throw new \RuntimeException('Function name is required');
+        $name = $values['name'] ?? $values['value'] ?? null;
+        if ($name === null) {
+            throw new RuntimeException('Function name is required');
         }
+        $this->setName($name);
+        $this->setSync($values['sync'] ?? $this->sync);
+        $this->setRange($values['range'] ?? false);
+        $this->setEval($values['eval'] ?? null);
+    }
 
-        $this->name = $name;
+    /**
+     *  Create nvim function spec from positional parameters
+     */
+    public static function createFunction(
+        string $name,
+        bool $sync = false,
+        bool $range = false,
+        string $eval = null
+    ) {
+        $values = [
+            'name' => $name,
+            'sync' => $sync,
+            'range' => $range,
+            'eval' => $eval,
+        ];
+        return new self($values);
     }
 
     public function getMethodName() : string
@@ -88,5 +110,31 @@ final class Func implements RpcSpec
     public function getShouldExport() : bool
     {
         return true;
+    }
+
+    private function setName(string $name)
+    {
+        if (empty($name)) {
+            throw new RuntimeException('Invalid function name');
+        }
+        $this->name = $name;
+    }
+
+    private function setSync(bool $sync)
+    {
+        $this->sync = $sync;
+    }
+
+    private function setRange(bool $range)
+    {
+        $this->opts['range'] = $range;
+    }
+
+    private function setEval(string $eval = null)
+    {
+        unset($this->opts['eval']);
+        if ($eval) {
+            $this->opts['eval'] = $eval;
+        }
     }
 }
--- test/Plugin/RpcHandler/FuncTest.php
+++ test/Plugin/RpcHandler/FuncTest.php
@@ -3,6 +3,8 @@
 namespace XerkusTest\Neovim\Plugin\RpcHandler;
 
 use PHPUnit_Framework_TestCase;
+use RuntimeException;
+use TypeError;
 use Xerkus\Neovim\Plugin\RpcHandler\Func;
 
 /**
@@ -14,18 +16,182 @@ class FuncTest extends PHPUnit_Framework_TestCase
 {
     /**
      * @covers ::__construct
+     * @covers ::getType
+     * @covers ::getName
+     * @covers ::getIsSync
+     * @covers ::getOpts
+     */
+    public function testAllValuesAreProperlySet()
+    {
+        $func = new Func([
+            'name' => 'TestFunc',
+            'sync' => true,
+            'range' => true,
+            'eval' => 'someEval()',
+        ]);
+        self::assertEquals('function', $func->getType());
+        self::assertEquals('TestFunc', $func->getName());
+        self::assertTrue($func->getIsSync());
+        self::assertEquals([
+            'range' => true,
+            'eval' => 'someEval()'
+        ], $func->getOpts());
+    }
+
+    /**
+     * @covers ::createFunction
+     * @covers ::getType
+     * @covers ::getName
+     * @covers ::getIsSync
+     * @covers ::getOpts
+     */
+    public function testNamedConstructorSetsAllValues()
+    {
+        $func = Func::createFunction('TestFunc', true, true,'someEval()');
+        self::assertEquals('function', $func->getType());
+        self::assertEquals('TestFunc', $func->getName());
+        self::assertTrue($func->getIsSync());
+        self::assertEquals([
+            'range' => true,
+            'eval' => 'someEval()'
+        ], $func->getOpts());
+    }
+
+    /**
+     * @covers ::__construct
      * @covers ::getSpecArray
      */
-    public function testSpecWithFuncNameOnly()
+    public function testRpcSpecArray()
     {
-        $func = new Func(['name' => 'TestFunc']);
-        self::assertEquals('TestFunc', $func->getName());
-        self::assertEquals('function:TestFunc', $func->getMethodName());
+        $func = new Func([
+            'name' => 'TestFunc',
+            'sync' => true,
+            'range' => true,
+            'eval' => 'someEval()',
+        ]);
         self::assertEquals([
             'type' => 'function',
             'name' => 'TestFunc',
-            'sync' => false,
-            'opts' => ['range' => false]
+            'sync' => true,
+            'opts' => ['range' => true, 'eval' => 'someEval()']
         ], $func->getSpecArray());
+    }
+
+    /**
+     * @covers ::getMethodName
+     */
+    public function testMethodNameIsCombinationOfTypeAndName()
+    {
+        $func = new Func(['name' => 'TestFunc']);
+        self::assertEquals('function:TestFunc', $func->getMethodName());
+    }
+
+    /**
+     * @covers ::__construct
+     */
+    public function testFunctionNameMustBeString()
+    {
+        $this->setExpectedExceptionRegExp(TypeError::class, '/must be of the type string/');
+        $func = new Func(['name' => true]);
+    }
+
+    /**
+     * @covers ::__construct
+     */
+    public function testFunctionNameMustNotBeEmpty()
+    {
+        $this->setExpectedException(RuntimeException::class, 'Invalid function name');
+        $func = new Func(['name' => '']);
+    }
+
+    /**
+     * @covers ::__construct
+     */
+    public function testFunctionNameIsRequired()
+    {
+        $this->setExpectedException(RuntimeException::class, 'Function name is required');
+        $func = new Func([]);
+    }
+
+    /**
+     * @covers ::__construct
+     */
+    public function testSyncMustBeBool()
+    {
+        $this->setExpectedExceptionRegExp(TypeError::class, '/must be of the type bool/');
+        $func = new Func(['name' => 'TestFunc', 'sync' => 1]);
+    }
+
+    /**
+     * @covers ::__construct
+     */
+    public function testRangeMustBeBool()
+    {
+        $this->setExpectedExceptionRegExp(TypeError::class, '/must be of the type bool/');
+        $func = new Func(['name' => 'TestFunc', 'range' => 1]);
+    }
+
+    /**
+     * @covers ::__construct
+     */
+    public function testEvalMustBeString()
+    {
+        $this->setExpectedExceptionRegExp(TypeError::class, '/must be of the type string/');
+        $func = new Func(['name' => 'TestFunc', 'eval' => 1]);
+    }
+
+    /**
+     * @covers ::__construct
+     */
+    public function testEvalIsNullable()
+    {
+        $func = new Func(['name' => 'TestFunc', 'eval' => null]);
+        self::assertArrayNotHasKey('eval', $func->getOpts());
+    }
+
+    /**
+     * @covers ::getPluginPath
+     */
+    public function testPluginPathIsNullByDefault()
+    {
+        $func = new Func(['name' => 'TestFunc']);
+        self::assertNull($func->getPluginPath());
+    }
+
+    /**
+     * @covers ::getPluginPath
+     * @covers ::withPluginPath
+     */
+    public function testWithPluginPathProducesNewCopy()
+    {
+        $func = new Func(['name' => 'TestFunc']);
+        $new = $func->withPluginPath('/test/path/plugin.php');
+        self::assertNotSame($new, $func);
+        self::assertNull($func->getPluginPath());
+        self::assertInstanceOf(Func::class, $new);
+        self::assertEquals('/test/path/plugin.php', $new->getPluginPath());
+    }
+
+    /**
+     * @covers ::getMethodName
+     * @covers ::withPluginPath
+     */
+    public function testPluginPathIsPrefixedToMethodName()
+    {
+        $func = new Func(['name' => 'TestFunc']);
+        $func = $func->withPluginPath('/test/path/plugin.php');
+        self::assertEquals(
+            '/test/path/plugin.php:function:TestFunc',
+            $func->getMethodName()
+        );
+    }
+
+    /**
+     * @covers ::getShouldExport
+     */
+    public function testFunctionIsExportedToNeovim()
+    {
+        $func = new Func(['name' => 'TestFunc', 'eval' => null]);
+        self::assertTrue($func->getShouldExport());
     }
 }
