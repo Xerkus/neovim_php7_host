176b9212b82c9a58a17aaf9fc5e4a40cf482c71c
Aleksei Khudiakov <aleksey@xerkus.pro>
2016-03-30 22:19:56 +1000

    Add plugin host entrypoint placeholder

--- README.md
+++ README.md
@@ -28,3 +28,33 @@ Coding styles are enforced with PHP_CodeSniffer. Checks can be run with
 `vendor/bin/phpcs` and some violations automatically fixed with
 `vendor/bin/phpcbf`
 
+# Warning
+
+Neovim uses plugin host process's stdin/stdout to open a channel. From help: 
+
+```
+rpcstart({prog}[, {argv}])                         {Nvim} *rpcstart()*
+        Spawns {prog} as a job (optionally passing the list {argv}),
+        and opens a |msgpack-rpc| channel with the spawned process's
+        stdin/stdout. It returns:
+          - The channel id on success, which is used by |rpcrequest()|,
+            |rpcnotify()| and |rpcstop()|
+          - 0 on failure.
+        Example: >
+            :let rpc_chan = rpcstart('prog', ['arg1', 'arg2'])
+```
+
+Any plugin running under this plugin host will break rpc channel and
+potentially cause some harm if it will try to output anything via STDOUT, echo,
+print or similar.
+
+At this time I do not have means to prevent or at least detect rogue output and
+neovim does not allow me to use sockets, not yet. As such I depend on and
+expect of plugin authors to ensure no output is made or it is captured and
+removed with output buffering.
+
+It can be done with this simple snippet, and it should not break nested buffers:
+
+```
+ob_start(function($string) { return '';});
+```
---
+++ bin/plugin-host.php
@@ -0,0 +1,51 @@
+#!/usr/bin/env zts-php
+<?php
+
+chdir(dirname(__DIR__));
+// Assert env meets minimum requirements
+if (!php_sapi_name() === 'cli') {
+    throw new RuntimeException('Only cli sapi is allowed');
+    exit(1);
+}
+if (PHP_MAJOR_VERSION !== 7) {
+    fwrite(STDERR, "PHP version 7 is required\n");
+    exit(1);
+}
+if (!in_array('pthreads', get_loaded_extensions())) {
+    fwrite(STDERR, "Pthreads extension is not loaded\n");
+    fwrite(STDERR, "Did you forget to install it or used non-zts php build?\n");
+    exit(1);
+}
+if (!in_array('msgpack', get_loaded_extensions())) {
+    fwrite(STDERR, "Plugin host needs msgpack extension to talk to neovim\n");
+    exit(1);
+}
+// I want to keep core plugin host at 0 external deps. Will try to get rid of
+// composer later
+if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    fwrite(STDERR, "Dependencies are not installed. Run composer install\n");
+    exit(1);
+}
+include __DIR__ . '/../vendor/autoload.php';
+
+//remove script path
+array_shift($argv);
+
+if (!count($argv)) {
+    fwrite(STDERR, "No plugins were specified\n");
+    exit(1);
+}
+
+try {
+    // currently only stdin/stdout server is supported by rpcstart()
+    // @TODO add logging
+    $session = new \Xerkus\Neovim\MsgpackRpc\Session(
+        new \Xerkus\Neovim\MsgpackRpc\Server\StdServer()
+    );
+    $pluginHost = new \Xerkus\Neovim\Plugin\Host($session);
+    $pluginHost->start($argv);
+} catch (\Throwable $e) {
+    fwrite(STDERR, (string)$e . "\n");
+    exit(1);
+}
+
